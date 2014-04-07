918b3737fa1f8ca20281f403a3b0cfacdc6d419d
hagerde <don_hager@trimble.com>
2014-04-07 11:32:49 -0600

    Pushing new changes

--- source/prep/collections/MovieLibrary.cs
+++ source/prep/collections/MovieLibrary.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace prep.collections
 {
@@ -19,42 +20,113 @@ namespace prep.collections
 
     public void add(Movie movie)
     {
-      throw new NotImplementedException();
+      bool found = false;
+      for (int i = 0; i < this.movies.Count; i++)
+      {
+        if ((this.movies[i].date_published == movie.date_published)
+            && (this.movies[i].genre == movie.genre)
+            && (this.movies[i].production_studio == movie.production_studio)
+            && (this.movies[i].rating == movie.rating)
+            && (this.movies[i].title == movie.title))
+        {
+          found = true;
+        }
+      }
+      if (!found)
+      {
+        this.movies.Add(movie);
+      }
     }
-    
+
     public IEnumerable<Movie> all_movies_published_by_pixar()
     {
-      throw new NotImplementedException();
+      IList<Movie> moviesToReturn = new List<Movie>();
+      for (int i = 0; i < this.movies.Count; i++)
+      {
+        if (this.movies[i].production_studio == ProductionStudio.Pixar)
+        {
+          moviesToReturn.Add(this.movies[i]);
+        }
+      }
+      return moviesToReturn.AsEnumerable();
     }
 
     public IEnumerable<Movie> all_movies_published_by_pixar_or_disney()
     {
-      throw new NotImplementedException();
+      IList<Movie> moviesToReturn = new List<Movie>();
+      for (int i = 0; i < this.movies.Count; i++)
+      {
+        if ((this.movies[i].production_studio == ProductionStudio.Pixar)||(this.movies[i].production_studio == ProductionStudio.Disney))
+        {
+          moviesToReturn.Add(this.movies[i]);
+        }
+      }
+      return moviesToReturn.AsEnumerable();
     }
 
     public IEnumerable<Movie> all_movies_not_published_by_pixar()
     {
-      throw new NotImplementedException();
+      IList<Movie> moviesToReturn = new List<Movie>();
+      for (int i = 0; i < this.movies.Count; i++)
+      {
+        if (this.movies[i].production_studio != ProductionStudio.Pixar)
+        {
+          moviesToReturn.Add(this.movies[i]);
+        }
+      }
+      return moviesToReturn.AsEnumerable();
     }
 
     public IEnumerable<Movie> all_movies_published_after(int year)
     {
-      throw new NotImplementedException();
+      IList<Movie> moviesToReturn = new List<Movie>();
+      for (int i = 0; i < this.movies.Count; i++)
+      {
+        if (this.movies[i].date_published.Year > year)
+        {
+          moviesToReturn.Add(this.movies[i]);
+        }
+      }
+      return moviesToReturn.AsEnumerable();
     }
 
     public IEnumerable<Movie> all_movies_published_between_years(int startingYear, int endingYear)
     {
-      throw new NotImplementedException();
+      IList<Movie> moviesToReturn = new List<Movie>();
+      for (int i = 0; i < this.movies.Count; i++)
+      {
+        if ((this.movies[i].date_published.Year >= startingYear) && (this.movies[i].date_published.Year <= endingYear))
+        {
+          moviesToReturn.Add(this.movies[i]);
+        }
+      }
+      return moviesToReturn.AsEnumerable();
     }
 
     public IEnumerable<Movie> all_kid_movies()
     {
-      throw new NotImplementedException();
+      IList<Movie> moviesToReturn = new List<Movie>();
+      for (int i = 0; i < this.movies.Count; i++)
+      {
+        if (this.movies[i].genre == Genre.kids)
+        {
+          moviesToReturn.Add(this.movies[i]);
+        }
+      }
+      return moviesToReturn.AsEnumerable();
     }
 
     public IEnumerable<Movie> all_action_movies()
     {
-      throw new NotImplementedException();
+      IList<Movie> moviesToReturn = new List<Movie>();
+      for (int i = 0; i < this.movies.Count; i++)
+      {
+        if (this.movies[i].genre == Genre.action)
+        {
+          moviesToReturn.Add(this.movies[i]);
+        }
+      }
+      return moviesToReturn.AsEnumerable();
     }
 
     public IEnumerable<Movie> sort_all_movies_by_title_descending()
